c15d7a452b3951fed2ec86fb00c64e3eff9d842a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2025-07-28 23:43:19 +0100

    Added filters and search options to the investment_product listing view. To filter the investments listing by category and/or type. The listing can be searched using the name or some words in the description

--- core/views/investment_product.py
+++ core/views/investment_product.py
@@ -1,12 +1,18 @@
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
 from ..models.investment_product import InvestmentProduct
 from ..serializers.investment_product_serializer import InvestmentProductSerializer
 
 class InvestmentProductListView(generics.ListAPIView):
     queryset = InvestmentProduct.objects.all()
     serializer_class = InvestmentProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'type']
+    search_fields = ['name', 'description']
+
 
 class InvestmentProductDetailView(generics.RetrieveAPIView):
     queryset = InvestmentProduct.objects.all()
     serializer_class = InvestmentProductSerializer
     lookup_field = 'id'
+
